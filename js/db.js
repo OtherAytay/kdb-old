64ca5f36645438b51c5206ef08719fe002028297
OtherAytay <OtherAytay@gmail.com>
2023-07-12 00:02:00 -0400

    Base DB schema

--- js/db.js
+++ js/db.js
@@ -2,5 +2,17 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('kinkDB');
 db.version(1).stores({
-    
+    // Organizational
+    categories: '++id, &name',
+    brands: '++id, name',
+    kinks: '++id, name',
+    groups: '++id, name',
+    labels: '++id, name',
+
+    // Items
+    dildos: '++id, name, brand, price',
+    anal_toys: '++id, name, brand, price',
+    bdsm: '++id, name, brand, price',
+    clothing: '++id, name, brand, price',
+    cosmetics: '++id, name, brand, price'
 })
